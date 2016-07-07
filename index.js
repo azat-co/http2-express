63904c895744f98dbf2de32b442e805c88dc3d9a
Azat Mardan <hi@azat.co>
2016-07-07 11:58:43 -0700

    add changes :mouse:

---
+++ index.js
@@ -0,0 +1,26 @@
+const port = 3000
+const spdy = require('spdy')
+const express = require('express')
+const path = require('path')
+const fs = require('fs')
+
+const app = express()
+
+app.get('*', (req, res) => {
+    res.json({foo: 'test'})
+})
+let options = {
+    key: fs.readFileSync(__dirname + '/server.key'),
+    cert:  fs.readFileSync(__dirname + '/server.crt')
+}
+console.log(options)
+spdy
+  .createServer(options, app)
+  .listen(port, (error) => {
+    if (error) {
+      console.error(error)
+      return process.exit(1)
+    } else {      
+      console.log('Listening on port: ' + port + '.')
+    }
+  })
